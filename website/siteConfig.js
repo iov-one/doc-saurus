fb564bbbd882c903b5652a7152d99ce9c0dab334
orkunkl <kulceorkun@gmail.com>
2019-07-01 19:11:08 +0300

    Add algolia and gh-pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,21 @@ const users = [
 const siteConfig = {
   title: 'Weave', // Title for your website.
   tagline: 'Welcome to weave framework!',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+ // url: 'https://your-docusaurus-test-site.com', // Your website URL
+ // baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  url: 'https://iov-one.github.io',
+  baseUrl: '/doc-saurus/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'doc-saurus',
+  organizationName: 'iov-one',
+
+  algolia: {
+    apiKey: process.env.ALGOLIA_API_KEY,
+    indexName: 'weave-tutorial',
+    algoliaOptions: {} // Optional, if provided by Algolia
+  },
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
